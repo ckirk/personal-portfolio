e6a009eb866f2daa31a435d8ee5b8f38320b1c64
Chris <ckirkinis@gmail.com>
2021-08-13 14:06:34 -0400

    removed extra cms stuff, added projects content

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,14 +1,14 @@
 import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare' // remove?
-import cloudinary from 'netlify-cms-media-library-cloudinary' // remove?
+// import uploadcare from 'netlify-cms-media-library-uploadcare' // remove?
+// import cloudinary from 'netlify-cms-media-library-cloudinary' // remove?
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
-CMS.registerMediaLibrary(uploadcare) // remove?
-CMS.registerMediaLibrary(cloudinary) // remove?
+// CMS.registerMediaLibrary(uploadcare) // remove?
+// CMS.registerMediaLibrary(cloudinary) // remove?
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)

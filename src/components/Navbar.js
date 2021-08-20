7a9446939243063cb16690d7fe0d4eeff2881235
Chris <ckirkinis@gmail.com>
2021-08-20 17:27:51 -0400

    added markdown video embed support

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/ck.svg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,8 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              {/* <img src={logo} alt="Kaldi" style={{ width: '88px' }} /> */}
-              <h1>CK</h1>
+              <img src={logo} alt="CK" style={{ maxHeight: '60px' }}/>
             </Link>
             {/* Hamburger menu */}
             <div
